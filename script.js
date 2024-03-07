0dfd76976b0869c031f05a081f43636d44778ed6
bagaskara2004 <bagaskaraputra87@gmail.com>
2024-03-07 10:12:27 +0800

    feat: add detail movie

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const inputSearch = document.getElementById('search');
 const btnSearch = document.getElementById('btnSearch');
 
-btnSearch.addEventListener("click",async function (e) {
+btnSearch.addEventListener("click", function (e) {
+    searchMovie(e)
+})
+
+async function searchMovie (e) {
     loading();
     try {
         const keyword = inputSearch.value;
@@ -12,7 +16,7 @@ btnSearch.addEventListener("click",async function (e) {
         const data = `<div class="error">${error}</div>`
         pushContent(data)
     }
-})
+}
 
 function getMovie(keyword) {
     return fetch("http://www.omdbapi.com/?apikey=1bf53760&s="+keyword).then(response => {
@@ -35,7 +39,7 @@ function makeCard(movies) {
         `<div class="card">
             <img src="${movie.Poster}">
             <p>${movie.Title}</p>
-            <button>DETAIL</button>
+            <button data-id="${movie.imdbID}" class="button">DETAIL</button>
         </div>`
     });
     return card;
@@ -44,6 +48,47 @@ function loading() {
     const data = `<div class="loading"></div>`;
     pushContent(data)
 }
+
+document.addEventListener('click',async function(e) {
+    if (e.target.classList.contains("button")) {
+        loading()
+        const id = e.target.dataset.id;
+        const detail = await getMovieDetail(id);
+        const data = makeDetail(detail);
+        pushContent(data);
+    }
+})
+
+function getMovieDetail(id) {
+    return fetch("http://www.omdbapi.com/?apikey=1bf53760&i="+id).then(response => {
+        return response.json()
+    }).then(response => response)
+}
+
+function makeDetail(detail) {
+    return `
+    <div class="detail">
+        <div class="content">
+            <img src="${detail.Poster}">
+            <ul>
+                <li><strong>Title :</strong>${detail.Title}</li>
+                <li><strong>Actors :</strong>${detail.Actors}</li>
+                <li><strong>Genre :</strong>${detail.Genre}</li>
+                <li><strong>Released :</strong>${detail.Released}</li>
+                <li><strong>Director :</strong>${detail.Director}</li>
+                <li><strong>Plot :</strong>${detail.Plot}</li>
+            </ul>
+        </div>
+        <button class="btnBack">Back</button>
+    </div>`
+}
+
+document.addEventListener('click',function (e) {
+    if (e.target.classList.contains("btnBack")) {
+        searchMovie(e)
+    }
+})
+
 function pushContent(data) {
     const content = document.querySelector('main').innerHTML = data;
 }
